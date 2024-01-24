adbae60c13c41de15a26d70be0f32bfd3e916bbf
ccoverstreet <coverst2@vols.utk.edu>
2024-01-24 12:07:20 -0500

    Added XRD Conversion script

---
+++ Rigaku_Lab_XRD/Rigaku.md
@@ -0,0 +1,10 @@
+# Rigaku Lab XRD
+
+This directory has a script that can be used to convert the output `.RAW` format files to a text `.xy` file. Jade Powder converter online or PowDLL also work as alternative methods of converting the file.
+
+## Maintanence
+
+## Troubleshooting
+
+- **CW** Warning
+    - Means water flow is insufficient for operating the XRD. This may indicate a blockage somewhere in the cooling system. Cleaning the tube filter (metal screen on top of XRD tube) will resolve this problem if no easily visible issue can be found.
---
+++ Rigaku_Lab_XRD/ccorawparser.py
@@ -0,0 +1,78 @@
+from io import SEEK_SET
+import struct
+import sys
+import matplotlib.pyplot as plt
+from dataclasses import dataclass
+import numpy as np
+from os import path
+
+# Header information starts at 0xBB8 offset
+# Data stream appears to start at 0xC02 offset
+
+def main():
+    for filename in sys.argv[1:]:
+        data = read_file(filename)
+
+        data.write_file(f"{path.splitext(path.basename(filename))[0]}_conv.xy")
+
+        plt.plot(data.two_theta, data.intensities)
+        plt.show()
+
+@dataclass
+class XRDData:
+    scan_speed: float
+    theta_min: float
+    theta_max: float
+    spacing: float
+    two_theta: np.array
+    intensities: np.array
+
+    def write_file(self, filename):
+        file = open(filename, "w")
+        file.write(f"# scan speed: {self.scan_speed}\n# theta min: {self.theta_min}\n# theta_max: {self.theta_max}\n# spacing: {self.spacing}\n# two theta, intensity\n")
+        for i in range(0, len(self.two_theta)):
+            file.write(f"{self.two_theta[i]} {self.intensities[i]} \n")
+
+        file.close()
+
+
+def read_file(filename):
+    f = open(filename, "rb")
+
+    # Get header info
+    f.seek(0xbb8)
+    scan_speed = struct.unpack("f", f.read(4))[0]
+    theta_min = struct.unpack("f", f.read(4))[0]
+    theta_max = struct.unpack("f", f.read(4))[0]
+    spacing = struct.unpack("f", f.read(4))[0]
+
+    print(scan_speed, theta_min, theta_max, spacing)
+
+
+    theta = []
+    intensities = []
+
+    f.seek(0xc02)
+    cur_loop = 0
+    while True:
+        block = f.read(4)
+        if len(block) < 4:
+            break
+
+        val = struct.unpack("f", block)[0]
+        intensities.append(val)
+        theta.append(theta_min + cur_loop * spacing)
+        cur_loop += 1
+
+
+    return XRDData(
+        scan_speed,
+        theta_min,
+        theta_max,
+        spacing,
+        np.array(theta),
+        np.array(intensities)
+    )
+
+if __name__ == "__main__":
+    main()

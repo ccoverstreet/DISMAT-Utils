b92b6004cc416fc3d6e093aafcad0d3c8dca1293
ccoverstreet <coverst2@vols.utk.edu>
2024-08-23 13:08:11 -0400

    Fixed line endings

--- SRIM/srim/srimdriver.py
+++ SRIM/srim/srimdriver.py
@@ -59,31 +59,31 @@ class SRIMConfig:
     max_energy: float
 
     def to_input_file_str(self):
-        buffer = "---Stopping/Range Input Data (Number-format: Period = Decimal Point)\r\n"
-        buffer += "---Output File Name\r\n"
-        buffer += f"\"{self.output_name}\"" + "\r\n"
-        buffer += "---Ion(Z), Ion Mass(u)\r\n"
-        buffer += f"{self.ion.atomic_number}   {self.ion.MAI_weight}\r\n"
-        buffer += "---Target Data: (Solid=0,Gas=1), Density(g/cm3), Compound Corr.\r\n"
-        buffer += f"{self.target_type.value} {self.density} {self.compound_corr}\r\n"
-        buffer += "---Number of Target Elements\r\n"
-        buffer += f"{len(self.stoich)}\r\n"
-        buffer += "---Target Elements: (Z), Target name, Stoich, Target Mass(u)\r\n"
+        buffer = "---Stopping/Range Input Data (Number-format: Period = Decimal Point)\n"
+        buffer += "---Output File Name\n"
+        buffer += f"\"{self.output_name}\"" + "\n"
+        buffer += "---Ion(Z), Ion Mass(u)\n"
+        buffer += f"{self.ion.atomic_number}   {self.ion.MAI_weight}\n"
+        buffer += "---Target Data: (Solid=0,Gas=1), Density(g/cm3), Compound Corr.\n"
+        buffer += f"{self.target_type.value} {self.density} {self.compound_corr}\n"
+        buffer += "---Number of Target Elements\n"
+        buffer += f"{len(self.stoich)}\n"
+        buffer += "---Target Elements: (Z), Target name, Stoich, Target Mass(u)\n"
         for i in range(0, len(self.stoich)):
             elem = self.elements[i]
-            buffer += f"{elem.atomic_number} \"{elem.name}\" {self.stoich[i]} {elem.natural_weight}\r\n"
+            buffer += f"{elem.atomic_number} \"{elem.name}\" {self.stoich[i]} {elem.natural_weight}\n"
 
-        buffer += "---Output Stopping Units (1-8)\r\n"
-        buffer += "5\r\n"
-        buffer += "---Ion Energy : E-Min(keV), E-Max(keV)\r\n"
-        buffer += f"{self.min_energy} {self.max_energy}\r\n"
+        buffer += "---Output Stopping Units (1-8)\n"
+        buffer += "5\n"
+        buffer += "---Ion Energy : E-Min(keV), E-Max(keV)\n"
+        buffer += f"{self.min_energy} {self.max_energy}\n"
 
         return buffer
 
 
 def run_srim_config(srim_config):
     sr_in = f"{PROGRAM_DIR}/srim/SR.IN"
-    with open(sr_in, "w") as f:
+    with open(sr_in, "w", newline="\r\n") as f:
         f.write(srim_config.to_input_file_str())
 
     subprocess.run(PROGRAM_DIR + "/srim/" + "SRModule.exe", cwd=PROGRAM_DIR + "/srim")

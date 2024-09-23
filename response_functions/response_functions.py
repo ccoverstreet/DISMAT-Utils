a0e695e596cb470a37b9b0a039d8c5ca770742cc
ccoverstreet <coverst2@vols.utk.edu>
2024-09-23 11:10:37 -0400

    Cleaned up testing files

--- response_functions/response_functions.py
+++ response_functions/response_functions.py
@@ -7,13 +7,16 @@ def linear(x, m, b):
 
 # y = 1 - exp(-phi * sigma)
 # Returns values between 0 and 1
-def single_impact(phi, sigma):
+def direct_impact(phi, sigma):
     return 1 - np.exp(-1 * phi * sigma)
 
 # y = f_sat * (1 - exp(-phi * sigma)) + offset
-def single_impact_scaled(phi, sigma, f_sat):
+def direct_impact_scaled(phi, sigma, f_sat):
     return f_sat * (1 - np.exp(-1 * phi * sigma))
 
 # y = f_sat * (1 - exp(-phi * sigma)) + offset
-def single_impact_arb(phi, sigma, f_sat, offset):
+def direct_impact_arb(phi, sigma, f_sat, offset):
     return offset + f_set * (1 - np.exp(-1 * phi * sigma))
+
+
+
